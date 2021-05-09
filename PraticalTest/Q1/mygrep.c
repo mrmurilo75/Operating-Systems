6640053c474fa2efe70eeb8059ccfac09cb66e87
mrmurilo75 <mrmurilo75@gmail.com>
2021-05-09 13:19:11 +0100

    @Q1 : finish

--- PraticalTest/Q1/mygrep.c
+++ PraticalTest/Q1/mygrep.c
@@ -10,27 +10,39 @@ typedef struct {
 
 
 int grep(FILE* file, String* toFind) {
-	char read = 1;
-	int line, col, cc = ' ', prev = cc;
+	int line, col, first, cc = '\0', prev = cc;
 
-	printf("\tbegin grep\n");
+	for(line = 1; cc != EOF; line++, cc= fgetc(file)) {
+		col = 1;
+		first = 1;
+		while(cc != EOF && cc != '\n') {
 
-	for(line = 1; cc != EOF; line++) {
-		printf("begin\n");
-		for(col = 1; cc != EOF && cc != '\n'; prev = cc, (read? cc = fgetc(file) : 0), col++ ) {
-			read = 1;
-					printf("%c\n", cc);
 			if(isspace(prev) && cc == toFind->value[0]) {
-				read = 0;
 				int colCandidate = col;
-				int i = 0;
+				int i = 1;
+
 				for(i = 1; i < toFind->length && (cc = fgetc(file)) == toFind->value[i]; i++, col++);
-				if(i == toFind->length && isspace(cc = fgetc(file)))
-					printf("[ %d: %d]\n", line, colCandidate);
+
+				if(i == toFind->length && isspace( (cc = fgetc(file)) )) {
+					col++;
+
+					if(first) {
+						printf("[ %d: ", line);
+						first = 0;
+					} else 
+						printf(", ");
+					printf("%d", colCandidate);
+				}
 			}
+
+			prev = cc;
+			if(cc != '\n')
+				cc = fgetc(file);
+			col++;
 		}
+		if(!first)
+			printf("]\n");
 	}
-	printf("\tend grep\n");
 
 	return 0;
 }
@@ -50,11 +62,7 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
-	printf("opening %s\n", argv[2]);
-
 	FILE* file = fopen(argv[2], "r");
-
-	printf("opened %s at %p\n", argv[2], file);
 
 	if(file == NULL) {
 		printf("unable to open file\n");
@@ -63,7 +71,7 @@ int main(int argc, char** argv){
 
 	String* toFind = newString(argv[1]);
 
-	printf("toFind.value = %s .length= %d\n", toFind->value, toFind->length);
+	file = fopen(argv[2], "r");
 
 	return grep(file, toFind);
 }
