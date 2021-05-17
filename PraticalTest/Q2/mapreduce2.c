af024c3a77d6a069399573799b8f18c4f2e8bf95
mrmurilo75 <mrmurilo75@gmail.com>
2021-05-17 21:53:07 +0100

    @PraticalTest/Q2 : working based on passing data as string (on mapreduce2, still needs clean up)

--- PraticalTest/Q2/mapreduce2.c
+++ PraticalTest/Q2/mapreduce2.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #define READ_END 0
 #define WRITE_END 1
@@ -18,7 +19,7 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	printf("\targc= %d\targv1= %s\targv2= %s\targv3= %s\t\n", argc, argv[1], argv[2], argv[3]);
+//	printf("\targc= %d\targv1= %s\targv2= %s\targv3= %s\t\n", argc, argv[1], argv[2], argv[3]);
 
 	FILE* file = fopen(argv[1], "r");
 	int n = parseIntFromFile(file),
@@ -34,7 +35,7 @@ int main(int argc, char* argv[]) {
 		i,
 		result;
 
-	printf("\tn= %d\tm= %d\tk= %d\n", n, m, k);
+//	printf("\tn= %d\tm= %d\tk= %d\n", n, m, k);
 
 	for(i = 0; i < m; i++) {
 
@@ -54,24 +55,24 @@ int main(int argc, char* argv[]) {
 
 	if(pid == 0) {	// child processing
 
-		printf("child= %d\ti= %d\n", pid, i);
+//		printf("child= %d\ti= %d\n", pid, i);
 
 		close(pipes[i][READ_END]);
 		result = 0;
 
 		for(int j = 0; j < n; j++) {
 
-			printf("\tj%%m= %d\n", j%m);
+//			printf("\tj%%m= %d\n", j%m);
 
 			if(j % m == i)
 				for(int e = 0; e < n; e++)
 					if(matriz[j][e] > k) {
-						printf("\telem= %d\n", matriz[j][e]);
+//						printf("\telem= %d\n", matriz[j][e]);
 						result++;
 					}
 		}
 
-		printf("\tresult= %d\n", result);
+//		printf("\tresult= %d\n", result);
 
 		/* child writes to pipe */
 
@@ -93,25 +94,31 @@ int main(int argc, char* argv[]) {
 	char line[BUF_SIZE];
 
 	/* parent reads from pipe */
-	for(i=0; i<m; i++) {
+	for(i = 0, result = 0; i < m; i++) {
 		close(pipes[i][WRITE_END]);
 
-	if ((nbytes = read(pipes[i][READ_END], line, BUF_SIZE)) < 0 ) {
-      fprintf(stderr, "Unable to read from pipe: %s\n", strerror(errno));                     
-      exit(EXIT_FAILURE);                                                                     
-    }                                                                                         
-    close(pipes[i][READ_END]);                                                                      
-    write(STDOUT_FILENO, line, nbytes);
+		if ((nbytes = read(pipes[i][READ_END], line, BUF_SIZE)) < 0 ) {
+			fprintf(stderr, "Unable to read from pipe: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		close(pipes[i][READ_END]);                 
 
-}
-    /* wait for child and exit */
-    for(int i =0; i <m; i++)
-    if ( wait(NULL) < 0) {
-      fprintf(stderr, "Unable to catch child exiting: %s\n", strerror(errno));       exit(EXIT_FAILURE);     
-    }
+//		write(STDOUT_FILENO, line, nbytes);
+		result+=parseIntFromString(line);
 
-    /* return gracefully */
-    exit(EXIT_SUCCESS);
+	}
+
+	printf("%d\n", result);
+
+	/* wait for child and exit */
+	for(int i =0; i <m; i++)
+		if ( wait(NULL) < 0) {
+			fprintf(stderr, "Unable to catch child exiting: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+	/* return gracefully */
+	exit(EXIT_SUCCESS);
  
 }
 
