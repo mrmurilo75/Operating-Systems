6be76cf85d300ab12509bf814dc9ecbff55ffa00
Murilo Rosa <mrmurilo75@gmail.com>
2021-05-19 13:33:06 +0100

    @PractialTest/Q2/mapreduce : found and fix error (pipes should be open bf forking) and finish implementing

--- PraticalTest/Q2/mapreduce.c
+++ PraticalTest/Q2/mapreduce.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #define READ_END 0
 #define WRITE_END 1
@@ -17,9 +18,8 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	printf("\targc= %d\targv1= %s\targv2= %s\targv3= %s\t\n", argc, argv[1], argv[2], argv[3]);
+//	printf("\targc= %d\targv1= %s\targv2= %s\targv3= %s\t\n", argc, argv[1], argv[2], argv[3]);
 
-	pid_t pid;
 	FILE* file = fopen(argv[1], "r");
 	int n = parseIntFromFile(file),
 		matriz[n][n];
@@ -27,13 +27,98 @@ int main(int argc, char* argv[]) {
 		for(int e = 0; e < n; e++)
 			matriz[j][e] = parseIntFromFile(file);
 
+	pid_t pid;
 	int m = parseIntFromString(argv[2]),
 		k = parseIntFromString(argv[3]),
 		pipes[m][2],
-		i;
+		i,
+		result;
 
-	printf("\tn= %d\tm= %d\tk= %d\n", n, m, k);
+//	printf("\tn= %d\tm= %d\tk= %d\n", n, m, k);
 
+	for(i = 0; i < m; i++) {
+
+		if (pipe(pipes[i]) < 0) {
+			perror("pipe error");
+			exit(EXIT_FAILURE);
+		}
+
+		if ((pid = fork()) < 0) {
+			perror("fork error");
+			exit(EXIT_FAILURE);
+		}
+
+		if(pid == 0) break; 	// child breaks out
+
+	}
+
+	if(pid == 0) {	// child processing
+
+//		printf("child= %d\ti= %d\n", pid, i);
+
+		close(pipes[i][READ_END]);
+		result = 0;
+
+		for(int j = 0; j < n; j++) {
+
+//			printf("\tj%%m= %d\n", j%m);
+
+			if(j % m == i)
+				for(int e = 0; e < n; e++)
+					if(matriz[j][e] > k) {
+//						printf("\telem= %d\n", matriz[j][e]);
+						result++;
+					}
+		}
+
+//		printf("\tresult= %d\n", result);
+
+		/* child writes to pipe */
+
+		if(write(pipes[i][WRITE_END], &result, sizeof(int)*sizeof(char)) < 0) {
+			perror("Unable to write to pipe");
+			exit(EXIT_FAILURE);
+		}
+
+		close(pipes[i][WRITE_END]);
+		exit(EXIT_SUCCESS);
+	}
+
+	// parent processing
+
+	int nbytes, current = 0;
+
+	/* parent reads from pipe */
+	for(i = 0, result = 0; i < m; i++) {
+		close(pipes[i][WRITE_END]);
+
+		if ((nbytes = read(pipes[i][READ_END], &current, sizeof(int)*sizeof(char))) < 0 ) {
+			fprintf(stderr, "Unable to read from pipe: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		close(pipes[i][READ_END]);                 
+
+//		write(STDOUT_FILENO, line, nbytes);
+		//result+=parseIntFromString(line);
+		result+=current;
+
+	}
+
+	printf("%d\n", result);
+
+	/* wait for child and exit */
+	for(int i =0; i <m; i++)
+		if ( wait(NULL) < 0) {
+			fprintf(stderr, "Unable to catch child exiting: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+	/* return gracefully */
+	exit(EXIT_SUCCESS);
+ 
+}
+
+/*
 	for (i = 0; i < m; i++) {
 		if( (pid = fork()) < 0 ){
 			perror("fork error");
@@ -102,6 +187,7 @@ int main(int argc, char* argv[]) {
 	printf("%d\n", result);
 	exit(EXIT_SUCCESS);
 }
+*/
 
 int parseIntFromFile(FILE* file) {
 	if(file == NULL)
